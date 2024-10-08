3a46b6eb88394f1efd086fa04b47ce4515cda834
Saka439 <133142291+Saka439@users.noreply.github.com>
2024-10-08 01:20:39 -0400

    Update ImcWindows.cpp

--- ImcWindows.cpp
+++ ImcWindows.cpp
@@ -1,6 +1,6 @@
 // La Vue-Controlleur pour l'afficheur d'Indice de Masse Corporelle
 // Par ndzana-missia.saka@polymtl.ca
-#pragma warning(push, 0) // Sinon Qt fait des avertissements à /W4.
+#pragma warning(push, 0) // Sinon Qt fait des avertissements Ã  /W4.
 #include "ImcWindows.h"
 #include "IMC.hpp"
 #include <iostream>
@@ -15,14 +15,14 @@ using iter::range;
 ImcWindow::ImcWindow(QWidget* parent) :
 	QMainWindow(parent)
 {
-	// Si on objet n'a pas encore de parent on lui met "this" comme parent en attendant, si possible, pour s'assurer que tous les pointeurs sont gérés par un delete automatique en tout temps sans utiliser de unique_ptr.
+	
 	auto widgetPrincipal = new QWidget(this);
 
 	setCentralWidget(widgetPrincipal);
 
-	auto layoutPrincipal = new QVBoxLayout(widgetPrincipal);  // Donner un parent à un layout est comme un setLayout.
+	auto layoutPrincipal = new QVBoxLayout(widgetPrincipal);  
 
-	// Ajout des QLabel pour entrer le poids et la taille
+
 	QLabel* poidsLabel = new QLabel("Poids en kg:");
 	nameEditor = new QLineEdit;
 	QHBoxLayout* poidsLayout = new QHBoxLayout;
@@ -49,19 +49,18 @@ ImcWindow::ImcWindow(QWidget* parent) :
 
 void ImcWindow::calculerImcEtAfficher()
 {
-	// Récupérez les valeurs du poids et de la taille
+	
 	double poids = nameEditor->text().toDouble();
 	double taille = nameEditor2->text().toDouble();
 
-	// Calculez l'IMC à l'aide de votre classe IMC
-	double resultat = imc.calculerImc1(); // ou imc.calculerImc2() en fonction de votre logique
+	
+	double resultat = imc.calculerImc1(); 
 	//double resultat2 = imc.calculerImc2();
 
-	// Affichez le résultat de l'IMC dans un QLabel
+	
 	if (layoutPrincipal) {
-		// Affichez le résultat de l'IMC dans un QLabel
+		
 		QLabel* resultatLabel = new QLabel("IMC: " + QString::number(resultat), this);
-		//setCentralWidget(resultatLabel);
 		layoutPrincipal->addWidget(resultatLabel);
 	}
 }
